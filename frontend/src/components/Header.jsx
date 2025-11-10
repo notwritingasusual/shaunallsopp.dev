32c31cc0cbf4a9aca64faa229bb0732c54a19357
Shaun Allsopp <shaunallsopp@Shauns-MacBook-Air.local>
2025-11-10 19:41:25 +0000

    padding profile images

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <img
                 src={profileImage}
                 alt="Shaun Allsopp"
-                className="w-24 h-25 sm: w-20 sm: h-20 object-cover border border-gray-300"
+                className="w-20 h-20 md:w-32 md:h-32 object-cover border border-gray-300"
             />
             <div className="flex flex-col ml-4">
                 <h1 className="text-lg font-bold font-mono text-[#556B2F]">
