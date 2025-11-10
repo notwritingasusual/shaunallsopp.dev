13f85a8f87403c0dc0570972c3d887b059e3fd13
Shaun Allsopp <shaunallsopp@Shauns-MacBook-Air.local>
2025-11-10 19:39:45 +0000

    Serve media files in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('api/', api.urls),
 ]
 
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files in debug mode
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
