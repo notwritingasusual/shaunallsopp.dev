815a86befa306e91768cb70c47bbc2b02ac67166
Shaun Allsopp <shaunallsopp@Shauns-MacBook-Air.local>
2025-11-11 21:52:33 +0000

    changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
                 className="w-20 h-20 md:w-32 md:h-40 object-cover border border-gray-300"
             />
             <div className="flex flex-col ml-4">
-                <h1 className="text-base font-bold font-mono text-[#556B2F]">
+                <h1 className="text-base font-bold font-mono text-[#556B2F] pt-1">
                     shaun
                 </h1>
                 <p className="text-sm font-bold text-gray-600">
@@ -28,7 +28,7 @@ function Header() {
                     </p>
                 </p>
 
-                <p className="text-sm font-mono text-gray-600">
+                <p className="text-sm font-mono text-gray-600 pt-1">
                     Available for employment (local or remote), freelance and collaborations.
                 </p>
             </div>
