cc318c8d052b8b2d02df242dfe36333babe8893b
Shaun Allsopp <shaunallsopp@Shauns-MacBook-Air.local>
2025-11-11 20:53:29 +0000

    get in touch side bar on larger screens only

--- frontend/src/components/GetInTouchSide.jsx
+++ frontend/src/components/GetInTouchSide.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 class GetInTouchSide extends React.Component {
     render() {
         return (
-            <section className="w-full flex flex-col items-start border-t border-gray-300 font-mono p-8 mt-10">
-                <h2 className="text-xl font-bold mb-4 text-[#556B2F]">contact</h2>
-                <p className="text-base font-mono text-sm text-gray-600 mb-2">
-                    I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions. Feel free to reach out!
-                </p>
+            <section className="md: border-l border-gray-300 lg: border-l border-gray-300 font-mono pl-4 ml-12 mt-7">
+
+
                 <p className="text-sm font-mono text-gray-600 mb-4">
                     email: <a href="mailto:shaunallsopp@outlook.com" className="text-[#556B2F] hover:underline text-sm font-bold">shaunallsopp@outlook.com</a>
                 </p>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import profileImage from '../assets/profile.jpeg';
+import GetInTouchSide from './GetInTouchSide';
 
 function Header() {
     return (
@@ -17,15 +18,21 @@ function Header() {
                     Web App Developer: East Midlands, UK.
                 </p>
                 <p className="text-sm font-mono text-gray-600 pt-6">
-                    Building modern, performant web applications.
+                    Building modern, performant web applications.     <p className="text-sm font-mono text-gray-600">
+                        <span className="font-bold text-[#556B2F]">Python</span>; Django;
+                        <span className="font-bold text-[#556B2F]">MySQL</span>;
+                        <span className="font-bold text-[#556B2F]">JavaScript</span>; React;
+                        <span className="font-bold text-[#556B2F]">HTML</span>;
+                        <span className="font-bold text-[#556B2F]">CSS</span>;
+                        <span className="font-bold text-[#556B2F]">TailwindCSS</span>.
+                    </p>
                 </p>
-                <p className="text-sm font-mono text-gray-600">
-                    <span className="font-bold text-[#556B2F]">Python</span>; Django; <span className="font-bold text-[#556B2F]">MySQL</span>; <span className="font-bold text-[#556B2F]">JavaScript</span>; React; <span className="font-bold text-[#556B2F]">HTML</span>; <span className="font-bold text-[#556B2F]">CSS</span>; <span className="font-bold text-[#556B2F]">TailwindCSS</span>.
-                </p>
+
                 <p className="text-sm font-mono text-gray-600">
                     Available for employment (local or remote), freelance and collaborations.
                 </p>
             </div>
+            <div className='hidden md:block'><GetInTouchSide /></div>
         </header>
     );
 }
